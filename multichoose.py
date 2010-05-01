271b512d3d9a94c432a7e29565dffa6c2f14c308
Erik Garrison <erik@hypervolu.me>
2010-05-01 09:33:11 -0400

    Python implementation now returns generator
    This should help in the case of the generation of many multiset
    combinations.

--- multichoose.py
+++ multichoose.py
@@ -30,25 +30,23 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 """
 
-def multichoose(k, objects):
-    """n multichoose k multisets from the list of objects.  n is the size of
-    the objects."""
-    choices = []
-    j,j_1,q = k,k,k  # init here for scoping
-    r = len(objects) - 1
-    a = [0 for i in range(k)] # initial multiset indexes
-    while True:
-        choices.append([objects[a[i]] for i in range(0,k)])  # emit result
-        j = k - 1
-        while j >= 0 and a[j] == r: j -= 1
-        if j < 0: break  # check for end condition
-        j_1 = j
-        while j_1 <= k - 1:
-            a[j_1] = a[j_1] + 1 # increment
-            q = j_1
-            while q < k - 1:
-                a[q+1] = a[q] # shift left
+    def multichoose(k, objects):
+        """n multichoose k multisets from the list of objects.  n is the size of
+        the objects."""
+        j,j_1,q = k,k,k  # init here for scoping
+        r = len(objects) - 1
+        a = [0 for i in range(k)] # initial multiset indexes
+        while True:
+            yield [objects[a[i]] for i in range(0,k)]  # emit result
+            j = k - 1
+            while j >= 0 and a[j] == r: j -= 1
+            if j < 0: break  # check for end condition
+            j_1 = j
+            while j_1 <= k - 1:
+                a[j_1] = a[j_1] + 1 # increment
+                q = j_1
+                while q < k - 1:
+                    a[q+1] = a[q] # shift left
+                    q += 1
                 q += 1
-            q += 1
-            j_1 = q
-    return choices
+                j_1 = q
